411baa8ab9a495f295db987aab70b3cfae8c2824
lazycoder1 <gautamgsabhahit@gmail.com>
2022-02-01 12:28:41 +0530

    modifying collectionList data

--- src/store/index.js
+++ src/store/index.js
@@ -158,10 +158,16 @@ export default new Vuex.Store({
       
       const collectList=[
         {
-          imageUrl:
+          image_url:
             "https://lh3.googleusercontent.com/F40quZ70BK_sefr3Np4seV9k-83tE5KpZ1gs-RxuQzWNYUDMPe-DOnLRdg3cZ_BxbJxa-mPBrq2FvC2YaxsCnlEhBGSZsScSdz1k=w286",
           name: "GENESIS_FUND",
-          returns: 15,
+          price: 50,
+          members: 50,
+          verified: true,
+          est_value: {
+            amount:50, currency:'$'},
+          symbol: 'GFUND',
+          chain: 'POLYGON',
           items: "2",
           contractId: "0xcB8D80AfDd6da10f77Fa7C1546250fe5e95279b5",
         },
@@ -169,7 +175,13 @@ export default new Vuex.Store({
           imageUrl:
             "https://lh3.googleusercontent.com/cqPy7mep0-LuTeiRBrrhYpZhNy60b8tiWnyzjx0aQ5kbAdrWYLpoieWzdcvSm8oNMV6c15gVMRQdDkJeDccHPQQP76rosXgOgDZJfM8=w286",
           name: "FUND2",
-          returns: 15,
+          price: 50,
+          members: 50,
+          verified: true,
+          est_value: {
+            amount:50, currency:'$'},
+          symbol: 'GFUND',
+          chain: 'POLYGON',
           items: "2",
           contractId: "0x99609Da05611A544DC918B0cC9e89b31D1e55BF1",
         },
