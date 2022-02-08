ef627d06cff04873e0d4a26c440962cff1a3c8c3
yash03112000 <yashag@iitk.ac.in>
2022-02-09 04:11:17 +0530

    switched to subscription method of multiple contracts

--- server/server.js
+++ server/server.js
@@ -32,92 +32,103 @@ app.use(function(req, res, next) {
 	next();
 });
 
-const web3 = new Web3(
-	Web3.givenProvider ||
-		'wss://rinkeby.infura.io/ws/v3/f0851ca048234e2a8b9fce6423433441'
-);
+// console.log(process.env.MUMBAI_WS);
 
-const CONTRACT_ADDRESS = '0x04d489aa53f8ded34066efca6747d7811a896bcd';
+const web3 = new Web3(Web3.givenProvider || process.env.ETH_WS);
 
-const contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+const CONTRACT_ADDRESS = '0x9c780C7a5b5a817431D3DCD71BE6F6198E4aA2b2';
+const CONTRACT_ADDRESS2 = '0x2c4C64fF4cFc16901EFB5aF6c4F2E2b9709651A8';
 
-async function getEvents(events) {
-	let latest_block = await web3.eth.getBlockNumber();
-	let historical_block = latest_block - 10000; // you can also change the value to 'latest' if you have a upgraded rpc
-	console.log('latest: ', latest_block, 'historical block: ', historical_block);
-	contract.events
-		.allEvents({}, function(error, event) {
-			// console.log('event');
-			// console.log(event);
-		})
-		.on('connected', function(subscriptionId) {
-			console.log('connected');
-			console.log(subscriptionId);
-		})
-		.on('data', async (event) => {
-			console.log('data');
-			console.log(event); // same results as the optional callback above
-			if (event.event === 'TokenPriceChanged') {
-				console.log('Price Change');
-				try {
-					const text = `update collections.collection_master cm set token_buy_price = $1 ,token_sell_price = $2 where cm.contract_address = $3 returning *;`;
-					const values = [
-						event.returnValues.value,
-						event.returnValues.value,
-						CONTRACT_ADDRESS,
-					];
-					console.log(event.returnValues.value);
-					const query = await pool.query(text, values);
-					console.log(query.rows);
-					// res.json({ a: query.rows });
-				} catch (err) {
-					console.log(err.stack);
+const getEvents = () => {
+	let options = {
+		fromBlock: null,
+		address: [CONTRACT_ADDRESS, CONTRACT_ADDRESS2], //Only get events from specific addresses,
+		topics: [],
+	};
+
+	let subscription = web3.eth.subscribe('logs', options);
+
+	subscription.on('data', (event) => {
+		console.log('event');
+		console.log(event);
+		const address = event.address;
+		const hash = event.transactionHash;
+		const contract = new web3.eth.Contract(abi, address);
+		contract
+			.getPastEvents(
+				'allEvents',
+				{
+					fromBlock: 0,
+					toBlock: 'latest',
+				},
+				function(error, events) {
+					// console.log(events);
 				}
-			} else if (event.event === 'TokensBought') {
-				console.log('TokensBought');
-				try {
-					const text = `update collections.collection_master cm set items = $1  where cm.contract_address = $2 returning *;`;
-					const currentquery =
-						'select * from collections.collection_master cm where cm.contract_address = $1;';
-					const currentvalues = [CONTRACT_ADDRESS];
-					const current = await pool.query(currentquery, currentvalues);
-					let curr_items = current.rows[0].items;
-					let new_items = curr_items + event.returnValues.amount;
-					const values = [new_items, CONTRACT_ADDRESS];
-					const query = await pool.query(text, values);
-					console.log(query.rows);
-				} catch (err) {
-					console.log(err.stack);
+			)
+			.then(async (events) => {
+				// console.log(events);
+				const curr = events.filter((e) => e.transactionHash === hash)[0];
+				console.log(curr);
+				if (curr.event === 'TokenPriceChanged') {
+					console.log('Price Change');
+					try {
+						const text = `update collections.collection_master cm set token_buy_price = $1 ,token_sell_price = $2 where cm.contract_address = $3 returning *;`;
+						const values = [
+							curr.returnValues.value,
+							curr.returnValues.value,
+							CONTRACT_ADDRESS,
+						];
+						console.log(curr.returnValues.value);
+						const query = await pool.query(text, values);
+						console.log(query.rows);
+						// res.json({ a: query.rows });
+					} catch (err) {
+						console.log(err.stack);
+					}
+				} else if (curr.event === 'TokensBought') {
+					console.log('TokensBought');
+					try {
+						const text = `update collections.collection_master cm set items = $1  where cm.contract_address = $2 returning *;`;
+						const currentquery =
+							'select * from collections.collection_master cm where cm.contract_address = $1;';
+						const currentvalues = [CONTRACT_ADDRESS];
+						const current = await pool.query(currentquery, currentvalues);
+						let curr_items = current.rows[0].items;
+						let new_items = curr_items + curr.returnValues.amount;
+						const values = [new_items, CONTRACT_ADDRESS];
+						const query = await pool.query(text, values);
+						console.log(query.rows);
+					} catch (err) {
+						console.log(err.stack);
+					}
+				} else if (curr.event === 'TokensSold') {
+					console.log('TokensSold');
+					try {
+						const text = `update collections.collection_master cm set items = $1  where cm.contract_address = $2 returning *;`;
+						const currentquery =
+							'select * from collections.collection_master cm where cm.contract_address = $1;';
+						const currentvalues = [CONTRACT_ADDRESS];
+						const current = await pool.query(currentquery, currentvalues);
+						let curr_items = current.rows[0].items;
+						let new_items = curr_items - curr.returnValues.amount;
+						const values = [new_items, CONTRACT_ADDRESS];
+						const query = await pool.query(text, values);
+						console.log(query.rows);
+					} catch (err) {
+						console.log(err.stack);
+					}
 				}
-			} else if (event.event === 'TokensSold') {
-				console.log('TokensSold');
-				try {
-					const text = `update collections.collection_master cm set items = $1  where cm.contract_address = $2 returning *;`;
-					const currentquery =
-						'select * from collections.collection_master cm where cm.contract_address = $1;';
-					const currentvalues = [CONTRACT_ADDRESS];
-					const current = await pool.query(currentquery, currentvalues);
-					let curr_items = current.rows[0].items;
-					let new_items = curr_items - event.returnValues.amount;
-					const values = [new_items, CONTRACT_ADDRESS];
-					const query = await pool.query(text, values);
-					console.log(query.rows);
-				} catch (err) {
-					console.log(err.stack);
-				}
-			}
-		})
-		.on('changed', function(event) {
-			// remove event from local database
-			console.log('changed');
-			console.log(event);
-		})
-		.on('error', function(error, receipt) {
-			// If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
-			console.log('error');
-			console.log(error);
-		});
-}
+			});
+	});
+	subscription.on('changed', (changed) => console.log(changed));
+	subscription.on('error', (err) => {
+		throw err;
+	});
+	subscription.on('connected', (nr) => {
+		console.log('connected');
+		console.log(nr);
+	});
+};
 
 getEvents();
 
