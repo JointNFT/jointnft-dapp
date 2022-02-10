34d3ca8f990147e6f8f9da7cb511d35b2336858b
lazycoder1 <gautamgsabhahit@gmail.com>
2022-02-10 20:08:30 +0530

    fix merge

--- src/components/fundDetails/FundDetails.vue
+++ src/components/fundDetails/FundDetails.vue
@@ -14,15 +14,6 @@
                   <v-img :src="getImg" height="30" width="30" style="padding:1px;"></v-img>
                   <br>
             <v-container>
-<<<<<<< HEAD
-       
-              <v-row>
-                <v-col style="font-family: PT Sans Caption ; font-weight:bold; padding:1px">Tokens in Circulation: {{ getCollectionDetails.totalSupply || 0 }} {{ getCollectionDetails.symbol }}</v-col>
-              </v-row>
-              <v-row>
-                <v-col style="font-family: PT Sans Caption ; font-weight:bold; padding:1px"> Tokens Owned: {{ getCollectionDetails.userTokenBalance }} {{ getCollectionDetails.symbol }}</v-col>
-              </v-row>
-=======
        <v-row>
         </v-row>
         <v-row>
@@ -31,7 +22,6 @@
         <v-row>
           <v-col style="font-family: PT Sans Caption ; font-weight:bold; padding:1px"> Tokens Owned: {{ getCollectionDetails.userTokenBalance }} {{ getCollectionDetails.symbol }}</v-col>
         </v-row>
->>>>>>> 2916be5 (fixing token symbol)
     
               <v-row>
                 <v-col style="font-family: PT Sans Caption; font-weight:bold; padding:1px"> Token Buy Price: {{ getCollectionDetails.tokenBuyPrice || 0 }} {{ getCurrency }}</v-col>
